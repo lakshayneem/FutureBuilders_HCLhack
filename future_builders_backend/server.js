548d231f7c6d379ac13e35da27af3fc6208fc042
kumaraman58748 <kumaraman58748@gmail.com>
2025-11-19 10:05:57 +0530

    add files in controller and model and  update .gitignore

--- future_builders_backend/server.js
+++ future_builders_backend/server.js
@@ -5,6 +5,12 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";   // ⬅️ NEW
+import appointmentRoutes from "./routes/appointmentRoutes.js";
+import doctorAppointmentRoutes from "./routes/doctorAppointmentRoutes.js";
+import patientAppointmentRoutes from "./routes/patientAppointmentRoutes.js";
+import availabilityRoutes from "./routes/availabilityRoutes.js";
+import doctorRoutes from "./routes/doctorRoutes.js";
+
 
 dotenv.config();
 const app = express();
@@ -16,7 +22,11 @@ connectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);        // ⬅️ NEW
-
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api/doctor/appointments", doctorAppointmentRoutes);
+app.use("/api/patient/appointments", patientAppointmentRoutes);
+app.use("/api/doctor/availability", availabilityRoutes);
+app.use("/api/doctors", doctorRoutes);
 app.get("/", (req, res) => {
   res.json({ message: "Future_Builders API is running" });
 });
